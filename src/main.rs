a3da0771640ec0a141c0570f6cd61c6a27499a96
Gregory Norton <gregory.norton@me.com>
2020-08-21 01:45:31 -0400

    Added references to rspotify

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate rspotify;
+
+use rspotify::client::Spotify;
+use rspotify::oauth2::SpotifyClientCredentials;
+use rspotify::senum::Country;
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    let client_credential = SpotifyClientCredentials::default().build();
+
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+
+    println!("{:?}", spotify);
 }
